58973cf2c9298bc09e76759cc0736551ca0b3e23
jx <jx@jvxiang.com>
2022-10-18 10:32:02 +0800

    初次提交

---
+++ ChromeTranslate/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ChromeTranslate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.btnQueryIp = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "修改的IP";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(98, 12);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(254, 28);
+            this.txtIp.TabIndex = 1;
+            // 
+            // btnQueryIp
+            // 
+            this.btnQueryIp.Location = new System.Drawing.Point(376, 6);
+            this.btnQueryIp.Name = "btnQueryIp";
+            this.btnQueryIp.Size = new System.Drawing.Size(122, 37);
+            this.btnQueryIp.TabIndex = 2;
+            this.btnQueryIp.Text = "查询推荐Ip";
+            this.btnQueryIp.UseVisualStyleBackColor = true;
+            this.btnQueryIp.Click += new System.EventHandler(this.btnQueryIp_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(56, 94);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(122, 37);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "确认修改";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(332, 94);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(122, 37);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 172);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnQueryIp);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "谷歌浏览器翻译Host修改";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnExit;
+
+        private System.Windows.Forms.Button btnQueryIp;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIp;
+
+        #endregion
+    }
+}
---
+++ ChromeTranslate/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PSHostsFile;
+
+namespace ChromeTranslate
+{
+    public partial class Form1 : Form
+    {
+        private const string TranslateAddress = "translate.googleapis.com";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var host = HostsFile.Get().FirstOrDefault(x => x.Hostname == TranslateAddress);
+            if (host != null)
+            {
+                txtIp.Text = host.Address;
+            }
+        }
+
+        private void btnQueryIp_Click(object sender, EventArgs e)
+        {
+            var ips = Dns.GetHostAddresses("translate.google.cn");
+            if (ips == null || ips.Length == 0)
+            {
+                MessageBox.Show("未找到合适的Ip，请手动查询");
+                return;
+            }
+
+            txtIp.Text = ips[0].ToString();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (txtIp.Text == "")
+            {
+                MessageBox.Show("Ip地址不能为空！");
+                return;
+            }
+
+            if (!IPAddress.TryParse(txtIp.Text, out _))
+            {
+                MessageBox.Show("您输入的Ip地址有误，请检查！");
+                return;
+            }
+
+            try
+            {
+                HostsFile.Set(TranslateAddress, txtIp.Text);
+                MessageBox.Show("修改成功，赶紧去试试吧。");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"修改失败了，大概可能是\"{exception.Message}\"的原因。");
+            }
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
